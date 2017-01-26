35028cf66dd8c27842e2af53fb0ddb955b861358
Girish Rawat <girishrawat@sfs-wifi-dhcp-10-143-174-94.wifi.sfsu.edu>
2017-01-26 13:48:11 -0800

    Assignment complete

--- Assignment 0/Main.java
+++ Assignment 0/Main.java
@@ -1,5 +1,16 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		System.out.println("Girish Rawat");
+		System.out.println("Freshman");
+		System.out.println("Computer Science");
+		System.out.println("To pass in order to get a degree.");
+		System.out.println("Yes. I hack on a Macbook Air 2015 with macOS Sierra.");
+		System.out.println("Yes, I've used both.");
+		System.out.println("I've programmed in Swift.");
+		System.out.println("6?");
+		System.out.println("Sublime Text as a text editor.");
+		System.out.println("I love video lectures on programming. That way I can code alongside the instructor.");
+		System.out.println("Not much. I've taken this class before but wasn't able to transfer it.");
+		System.out.println("Do you offer extra credit?");
 	}
 }
