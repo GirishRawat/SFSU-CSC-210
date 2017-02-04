ee0c2f87a10b26941802898772eccbe74349b1e0
Girish Rawat <Emailgirishrawat@gmail.com>
2017-02-04 15:28:16 -0800

    Spaces instead of tabs and inclusivity check

--- Assignment 1/Gpa.java
+++ Assignment 1/Gpa.java
@@ -8,5 +8,12 @@ public class gpa {
 		System.out.println("How many classes did you take?");
 		System.out.println("(Enter a number between 4 and 7)");
 		numberOfClasses = reader.nextInt();
+
+		// Check if numberOfClasses is between 4 and 7
+		while (!(numberOfClasses>4 && numberOfClasses<7)) {
+			System.out.println("Please enter a number between 4 and 7");
+			System.out.println("How many classes did you take?");
+			numberOfClasses = reader.nextInt();
+		} 
 	} 
 }
