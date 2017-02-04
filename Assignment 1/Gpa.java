1b99f2d8709c56e831f3ff9452dd413ac9c2213b
Girish Rawat <Emailgirishrawat@gmail.com>
2017-02-04 15:31:57 -0800

    Uggh I need more spaces over tabs

--- Assignment 1/Gpa.java
+++ Assignment 1/Gpa.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class gpa {
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		int numberOfClasses;
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numberOfClasses;
 
-		// Ask for number of classes	
-		System.out.println("How many classes did you take?");
-		System.out.println("(Enter a number between 4 and 7)");
-		numberOfClasses = reader.nextInt();
+    // Ask for number of classes  
+    System.out.println("How many classes did you take?");
+    System.out.println("(Enter a number between 4 and 7)");
+    numberOfClasses = reader.nextInt();
 
-		// Check if numberOfClasses is between 4 and 7
-		while (!(numberOfClasses>4 && numberOfClasses<7)) {
-			System.out.println("Please enter a number between 4 and 7");
-			System.out.println("How many classes did you take?");
-			numberOfClasses = reader.nextInt();
-		} 
-	} 
+    // Check if numberOfClasses is between 4 and 7
+    while (!(numberOfClasses>4 && numberOfClasses<7)) {
+      System.out.println("Please enter a number between 4 and 7");
+      System.out.println("How many classes did you take?");
+      numberOfClasses = reader.nextInt();
+    } 
+  } 
 }
