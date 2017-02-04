74bcf7610c15e834a42e314e847f12d2f3341143
Girish Rawat <Emailgirishrawat@gmail.com>
2017-02-04 15:19:01 -0800

    Added Gpa.java, started taking inputs

---
+++ Assignment 1/Gpa.java
@@ -0,0 +1,12 @@
+import java.util.scanner
+public class gpa {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		int numberOfClasses;
+
+		// Ask for number of classes	
+		System.out.println("How many classes did you take?");
+		System.out.println("(Enter a number between 4 and 7)");
+		numberOfClasses = reader.nextInt();
+	} 
+}
