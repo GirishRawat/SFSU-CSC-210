701f2088592f11fe0fd6494cefe3bac794607c1a
Girish Rawat <Emailgirishrawat@gmail.com>
2017-02-17 23:50:10 -0800

    added booleans for part 1

--- Assignment 2/Quiz.java
+++ Assignment 2/Quiz.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Quiz {
-	
+	public static void main (String[] args) {
+    boolean op1, op2, op3;
+    Scanner reader = new Scanner(System.in);
+
+    // Part 1
+    while (true) {
+     System.out.println("2+2=4?");
+     System.out.println("2+3=5?");
+     System.out.println("2+5=18?");
+    }
+	}
 }
